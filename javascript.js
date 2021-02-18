b6bcc268161bde189392482770051c6e2766d4f6
DmitryASU <77631908+DmitryASU@users.noreply.github.com>
2021-02-18 20:19:22 +0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-//alert( 'Добро пожаловать на портал Itrobo' );
+//alert ('Добро пожаловать на портал Itrobo');
+
+
+var i = 26;
+
+function indicator(n) {
+	//var i = document.querySelector(".ind_temp").value;
+	//var i = document.querySelectorA("span[id^=\'ind_temp\']")
+     
+	   i = i + n;
+	   ind_temp.innerHTML = i;
+}
+//windon.onclick;
